bf29b01148c10e839dc6510df10b56c100506171
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-07-09 14:03:04 +0500

    file upload

--- mini_project_17/app.js
+++ mini_project_17/app.js
@@ -5,15 +5,38 @@ const postModel = require("./models/post");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const crypto = require("crypto");
+const path = require("path");
+const multerconfig = require("./config/multerconfig");
+const user = require('./models/user');
+const upload = multerconfig; 
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.render("index");
 });
+
+app.get('/profile/upload', (req, res) => {
+    res.render("profileupload");
+});
+
+app.post('/upload', isLoggedIn, upload.single("image"), async (req, res) => {
+ let user = await userModel.findOne({email: req.user.email});
+  user.profilepic = req.file.filename;
+  await user.save()
+  res.redirect("/profile")
+  
+});
+
+
+
+
 
 app.get('/login', (req, res) => {
     res.render("login");
