763d5b07c8ea6cc470f401c99b3debabc2e2b546
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-07-10 15:57:54 +0500

    chess

--- Chess.com/app.js
+++ Chess.com/app.js
@@ -22,7 +22,50 @@ app.get("/", (req, res)=>{
 
 io.on("connection", function(uniquesocket){
 console.log("connected");
-})
+
+if(!players.white){
+    players.white= uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+}
+else if (!players.black){
+    players.black= uniquesocket.id;
+    uniquesocket.emit("playerRole", "b");
+}
+else{
+    uniquesocket.emit("spectatorRole");
+}
+
+uniquesocket.on("disconnect", function(){
+    if(uniquesocket.id=== players.white){
+        delete players.white;
+    }else if(uniquesocket.id === players.black){
+        delete players.black;
+    }
+});
+
+uniquesocket.on("move", (move)=>{
+    try{
+        if(chess.turn() === 'w' && uniquesocket.id !== players.white)return;
+        if(chess.turn() === 'b' && uniquesocket.id !== players.black)return;
+
+        const result = chess.move(move);
+        if(result){
+            currentPlayer = chess.turn();
+            io.emit("move", move);
+            io.emit("boardState", chess.fen());
+        }
+        else{
+            console.log("invalid move :", move);
+            uniquesocket.emit("invalidMove", move);
+        }
+    }
+    catch(err){
+        console.log(err);
+        uniquesocket.emit("Invalid move: ", move);
+    }
+});
+
+});
 
 server.listen(3000, function(){
 console.log("listening on port 3000");
